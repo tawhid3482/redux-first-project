a27aa71ab192dd589f6ad9f88a27c99ccfcdca40
saikat <tawhidulislam3482@gmail.com>
2024-04-22 19:37:54 +0600

    redux counter

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+    const {} = useSelector((state)=> state.counter)
     return (
         <div>
-            
+            <button>increment</button>
+            <div className="">
+                <h1>0</h1>
+            </div>
+            <button>decrement</button>
         </div>
     );
 };
--- src/Redux/features/counter/counterSlice.js
+++ src/Redux/features/counter/counterSlice.js
@@ -5,7 +5,15 @@ const initialState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.count = state.count += 1
+    },
+    decrement: (state) => {
+      state.count = state.count -= 1
+    }
+  },
 });
 
+export const { increment, decrement,  } = counterSlice.actions
 export default counterSlice.reducer;
