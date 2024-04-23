77fa38537d0d47d588d66ed216c38ca6a4380399
saikat <tawhidulislam3482@gmail.com>
2024-04-23 10:45:27 +0600

    reduc logger install

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Counter from "../Components/Counter";
 const Home = () => {
     return (
         <div>
-            <p className="text-3xl text-center">Home</p>
+            <p className="text-3xl text-center">Home page</p>
             <Counter/>
         </div>
     );
--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counter/counterSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+import logger from 'redux-logger'
 const store = configureStore({
-    reducer:{
-        counter : counterReducer,
-    },
-})
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 export default store;
