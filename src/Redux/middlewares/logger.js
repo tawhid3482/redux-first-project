6ce65e3d39fc376378c76d7ba2ff1c75e3959245
saikat <tawhidulislam3482@gmail.com>
2024-04-23 11:08:33 +0600

    redux logger ready

--- src/Redux/middlewares/logger.js
+++ src/Redux/middlewares/logger.js
@@ -1,9 +1,8 @@
 const logger = (store) => (next) => (action) => {
   const currentState = store.getState();
-  console.log("Current State =>",currentState)
-  console.log("Action dispatched =>",action)
-  next(action)
-  console.log("updated State =>",store.getState())
-
+  console.log("Current State =>", currentState);
+  console.log("Action dispatched =>", action);
+  next(action);
+  console.log("updated State =>", store.getState());
 };
 export default logger;
